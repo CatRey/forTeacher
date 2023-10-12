79b2732b51f95a345dab8f8b6e7f19a153120608
CatRey <interes_tv@inbox.ru>
2023-10-12 12:37:29 +0300

    rfgh

--- tt/src/App.java
+++ tt/src/App.java
@@ -1,5 +1,9 @@
+import java.until.*;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        System.out.println(n);
     }
 }
